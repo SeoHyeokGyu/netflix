de4608f7cf298a6a96f252e12ee1865f008e4f26
SeoHyeokGyu <shk64099@gmail.com>
2021-05-18 01:05:35 +0900

    #route 작성, Pages/Home,Browse,Signin, Signup

---
+++ src/pages/home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {JumbotronContainer} from "../containers/jumbotron";
+import {FaqContainer} from "../containers/faq";
+import {FooterContainer} from "../containers/footer";
+
+export default function Home(){
+    return (
+        <>
+            <JumbotronContainer/>
+            <FaqContainer/>
+            <FooterContainer/>
+        </>
+    );
+};
