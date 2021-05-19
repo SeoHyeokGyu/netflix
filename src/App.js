900cecae79166de2636327795850e9e9309dbd11
SeoHyeokGyu <shk64099@gmail.com>
2021-05-19 20:32:59 +0900

    #Helper: react routes

--- src/App.js
+++ src/App.js
@@ -2,19 +2,24 @@ import React from "react";
 import {BrowserRouter as Router, Route} from 'react-router-dom'
 import * as ROUTES from './constants/routes'
 import {Home, Browse, SignIn, SignUp} from "./pages";
+import {IsUserRedirect} from "./helpers/routes";
 
 export default function App() {
+    const user = {};
+
     return (
         <Router>
             <Route exact path="/browse">
                 <Browse/>
             </Route>
-            <Route exact path="/signin" >
+
+            <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.SIGN_IN} exact>
                 <SignIn/>
-            </Route>
-            <Route exact path="/signup">
+            </IsUserRedirect>
+            <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.SIGN_UP} exact>
                 <SignUp/>
-            </Route>
+            </IsUserRedirect>
+
             <Route exact path={ROUTES.HOME}>
                 <Home/>
             </Route>
