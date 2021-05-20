7af3f19b7021b6aad597ddd791da48c6e1221944
SeoHyeokGyu <shk64099@gmail.com>
2021-05-20 17:44:01 +0900

    #Profiles component 1. Loading profile component

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -1,7 +1,24 @@
-import React from "react";
+import React, {useContext, useEffect, useState} from "react";
 import {SelectProfileContainer} from "./profiles";
+import {FirebaseContext} from "../context/firebase";
+import {Loading} from '../components'
 
 export function BrowseContainer({slides}) {
 
-    return <SelectProfileContainer />
+    const [profile, setProfile] = useState({});
+    const [loding, setLoding] = useState(true);
+    const {firebase} = useContext(FirebaseContext);
+    const user = firebase.auth().currentUser || {};
+
+    useEffect(() => {
+
+        setTimeout(() => {
+            setLoding(false);
+        }, 3000);
+    }, [profile.displayName]);
+
+    return profile.displayName ? (
+            loding ? <Loading src={user.photoURL}/> : null)
+        : <SelectProfileContainer user={user} setProfile={setProfile}/>
+
 }
--- src/containers/profiles.js
+++ src/containers/profiles.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Header} from "../components";
+import {Header, Profiles} from "../components";
 import * as ROUTES from '../constants/routes'
 import logo from '../logo.svg'
 
@@ -7,20 +7,24 @@ export function SelectProfileContainer({user, setProfile}) {
 
     return (
         <>
-        <Header bg={false}>
-            <Header.Frame>
-                <Header.Logo to={ROUTES} src={logo} alt="Netfilx" />
-            </Header.Frame>
-        </Header>
+            <Header bg={false}>
+                <Header.Frame>
+                    <Header.Logo to={ROUTES} src={logo} alt="Netfilx"/>
+                </Header.Frame>
+            </Header>
             <Profiles>
                 <Profiles.Title>Who's watching?</Profiles.Title>
                 <Profiles.List>
-                    <Profile.User>
-                        <Profiles.Picture/>
-                        <Profiles.Name></Profiles.Name>
-                    </Profile.User>
+                    <Profiles.User onClick={() => setProfile({
+                        displayName: user.displayName,
+                        photoURL: user.photoURL,
+                    })
+                    }>
+                        <Profiles.Picture src={user.photoURL}/>
+                        <Profiles.Name>{user.displayName}</Profiles.Name>
+                    </Profiles.User>
                 </Profiles.List>
             </Profiles>
-    </>
+        </>
     );
 }
