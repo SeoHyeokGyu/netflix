0c76f5cd6def155e79a2c4983fd3d3907b7aaac6
SeoHyeokGyu <shk64099@gmail.com>
2021-05-20 16:42:41 +0900

    #util directory 1. selectionFilter

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import {useContent} from "../hooks";
+import selectionFilter from "../utils/selection-filter";
 
 export default function Browse() {
     const {series} = useContent('series');
     const {films} = useContent('films');
 
+    const slides = selectionFilter({series,films});
 
+    console.log(slides);
 
     return (<div>
             browse</div>
